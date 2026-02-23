f51f95dd72770e905d86fa1f163397d3f3decc75
sa5882218@gmail.com <sa5882218@gmail.com>
2026-02-23 09:25:25 +0500

    ads script for ads

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,7 +6,7 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6218367996393783"
+         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6218367996393783"
      crossorigin="anonymous"></script>
         </Head>
         <body>
